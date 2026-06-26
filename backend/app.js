0921b941e4a2a1235cc6c36364c386504a1acfa3
Biswajit-45-Star <officialbiswajit45@gmail.com>
2026-06-27 00:19:06 +0530

    fix: update CORS configuration to use allowedOrigins array

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,14 @@ connectDB();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://biswajit-sahu-v2.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
